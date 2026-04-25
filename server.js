fc727dfce134dba0747ba99c53d96152213a0b50
supriya_sinha <supriyasinha.dar@gmail.com>
2026-04-25 09:44:56 +0530

    Updated env

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const path = require('path');
 const zlib = require('zlib');
 const { EventEmitter } = require('events');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
